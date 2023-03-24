509ef1a5e9798cde0084318499fa3b8b13159d92
0xkabir <kabir.sadiken1@gmail.com>
2023-03-24 16:48:54 +0600

    removed Material UI and included Tailwind CSS with DaisyUI

--- src/components/shared/NextNav.jsx
+++ src/components/shared/NextNav.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const NextNav = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      
+    </div>
+  );
 };
 
 export default NextNav;
