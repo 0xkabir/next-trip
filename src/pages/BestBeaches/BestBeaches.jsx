3bc479c4199d4473654c9a7875747697439128cf
Shamimayesmin <shamimayesmin144@gmail.com>
2023-03-25 00:53:29 +0600

    new add

---
+++ src/pages/BestBeaches/BestBeaches.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BestBeaches = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BestBeaches;
--- src/pages/NextHoliday/NextHoliday.jsx
+++ src/pages/NextHoliday/NextHoliday.jsx
@@ -10,7 +10,7 @@ const NextHoliday = () => {
 		<div className="hero h-96 bg-no-repeat bg-cover w-full"  style={{ backgroundImage: `url(${img3})`, }}>
 			<div className="hero-overlay bg-opacity-30"></div>
 			<div className="hero-content text-justify items-center text-neutral-content">
-				<div className=" flex">
+				<div className="flex flex-col lg:flex-row">
 					<h1 className="mb-5 text-5xl font-bold">Your <br /> Next <span className="underline text-pink-500">Holiday</span></h1>
                    
 					<button className="btn bg-white text-pink-400 ml-20 mt-8 rounded-3xl">View all Package</button>
