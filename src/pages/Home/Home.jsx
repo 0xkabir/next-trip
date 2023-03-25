8675e099a42b21d34382afa082afc3e5c6125207
0xkabir <kabir.sadiken1@gmail.com>
2023-03-26 00:46:17 +0600

    added banner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import BestBeaches from '../BestBeaches/BestBeaches';
 import NextHoliday from '../NextHoliday/NextHoliday';
 import WorldTour from '../WorldTour/WorldTour';
+import Slider from './Slider';
 
 const Home = () => {
     return (
         <div>
-          
+        <Slider/>
         <NextHoliday></NextHoliday>
         <BestBeaches></BestBeaches>
         <WorldTour></WorldTour>
---
+++ src/pages/Home/Slider.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import HeroSlider, {Slide} from 'hero-slider';
+import image1 from '../../assets/slider/slider_1.jpg'
+import image2 from '../../assets/slider/slider_2.jpg'
+import image3 from '../../assets/slider/slider_3.jpg'
+
+const Slider = () => {
+  return (
+    <HeroSlider
+        autoplay
+        height="70vh"
+    >
+      {/* <Overlay>
+        <Wrapper>
+          <Title>Zoom Background Animations</Title>
+          <Subtitle>
+            The <code>backgroundAnimation</code> prop of the <code>Slide</code>{' '}
+            child components is set to <code>zoom</code>.
+          </Subtitle>
+        </Wrapper>
+      </Overlay> */}
+      <Slide
+        label="Hallstatt - Austria"
+        background={{
+          backgroundImageSrc: image1,
+          backgroundAnimation: 'zoom'
+        }}
+      />
+
+      <Slide
+        label="Hvitserkur - Iceland"
+        background={{
+          backgroundImageSrc: image2,
+          backgroundAnimation: 'zoom'
+        }}
+      />
+
+      <Slide
+        label="Jacksonville - USA"
+        background={{
+          backgroundImageSrc: image3,
+          backgroundAnimation: 'zoom'
+        }}
+      />
+    </HeroSlider>
+  );
+};
+
+export default Slider;
