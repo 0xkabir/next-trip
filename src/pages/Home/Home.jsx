ff8c2309036494997be29e7b8759ddac2815bd2a
Antu_Das <antuchandradasp9@gmail.com>
2023-03-26 00:07:13 +0600

    Category Section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import EventsTourCard from '../../components/Cards/EventsTourCard';
+import CategorySection from '../CategorySection/CategorySection';
+import DiscoverSection from '../DiscoverSection/DiscoverSection';
 import NextHoliday from '../NextHoliday/NextHoliday';
 
 const Home = () => {
     return (
         <div>
+            {/* Categories */}
+            <CategorySection />
             {/* <h1>This is Home Page</h1> */}
-        <NextHoliday></NextHoliday>
+            <NextHoliday></NextHoliday>
+            {/* Discover Section */}
+            <DiscoverSection />
+            {/* Event Tour Card */}\
+            <EventsTourCard />
         </div>
     );
 };
