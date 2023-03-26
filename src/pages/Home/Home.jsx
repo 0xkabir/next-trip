16b70806282486b3655cb5386e28bb5b8d77572d
Antu_Das <antuchandradasp9@gmail.com>
2023-03-26 23:38:13 +0600

    Home Changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,13 +5,13 @@ import BestBeaches from '../BestBeaches/BestBeaches';
 import NextHoliday from '../NextHoliday/NextHoliday';
 import WorldTour from '../WorldTour/WorldTour';
 import OurBestPackagesHome from '../OurBestPackagesHome/OurBestPackagesHome';
-// import Slider from './Slider';
+import Slider from './Slider';
 
 const Home = () => {
     return (
         <div>
             {/* Slider Section */}
-            {/* <Slider /> */}
+            <Slider />
             {/* Categories */}
             <CategorySection />
             {/* <h1>This is Home Page</h1> */}
